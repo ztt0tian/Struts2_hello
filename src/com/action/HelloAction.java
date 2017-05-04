8b742cfddd2f9c1aebe5784d1c7ac75d7fa4e009
ztt0tian <12950507445@qq.com>
2017-05-04 15:57:50 +0800

    Struts2_hello

---
+++ src/com/action/HelloAction.java
@@ -0,0 +1,18 @@
+package com.action;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class HelloAction extends ActionSupport{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+
+}
